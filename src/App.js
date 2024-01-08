483d1f6eacfd935b33badab05e01d247d2fac889
LEE HYEONSOO <dlgustn96@timesoft.co.kr>
2024-01-08 13:51:38 +0900

    basename={process.env.PUBLIC_URL} 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
